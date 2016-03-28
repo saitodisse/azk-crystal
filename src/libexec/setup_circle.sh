3b42677fbfd9cbc500cf01b6a4813edb84143a38
Everton Ribeiro <nuxlli@gmail.com>
2016-03-28 16:18:12 -0300

    Adding circle ci configure

---
+++ src/libexec/setup_circle.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [[ ! -e shards/bin/shards ]]; then
+  if [[ ! -d crystal-lang/shards ]]; then
+    git clone https://github.com/crystal-lang/shards
+  fi
+
+  cd shards
+  crystal build src/shards.cr -o bin/shards --release
+fi
+
+cp bin/shards /usr/local/bin
