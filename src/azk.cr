279e0acfa3a6c944d72dbf5c9c5e2d8e583343ed
Everton Ribeiro <nuxlli@gmail.com>
2016-03-29 02:06:58 -0300

    Testing embed js with macro

--- src/azk.cr
+++ src/azk.cr
@@ -6,9 +6,20 @@ require "file"
 module Azk
   extend self
 
+  ifdef release
+    macro js_content(dir, file)
+      \{{ run("./azk/build_utils/js_content", {{dir}}, {{file}}) }}
+    end
+  else
+    macro js_content(dir, file)
+      %file = File.join({{dir}}, {{file}})
+      File.read(%file)
+    end
+  end
+
   def main
     sbx = Duktape::Sandbox.new
-    sbx.eval! File.read("./share/main.js")
+    sbx.eval! js_content(__DIR__, "../share/main.js")
   end
 end
 
